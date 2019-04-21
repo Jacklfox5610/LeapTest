730e3917d92f7e29ad0603309b6e67f7ebb33eb5
taihei55 <ganth36@gmail.com>
2019-04-21 17:14:06 +0900

    LeapTest
    test

---
+++ Assets/LeapManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeapManager : MonoBehaviour
+{
+    Exercise exercise;
+    Leap.Unity.LeapServiceProvider leapServiceProvider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        leapServiceProvider.Load(exercise.recording);
+        Debug.Log(leapServiceProvider.CurrentFrame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
